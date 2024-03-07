65a4c79542ad147b5b984bd652ed5519fe9a4990
username-kunalg <kunalg.tracencode@gmail.com>
2024-03-07 16:53:53 +0530

    remove index page

--- record_keeper/urls.py
+++ record_keeper/urls.py
@@ -3,12 +3,12 @@ from .views import *
 
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
+    path('index/', IndexView.as_view(), name='index'),
     path('records/', RecordListView.as_view(), name='record_list'),
     path('records/create/', RecordCreateView.as_view(), name='record_create'),
     path('records/<int:pk>/edit/', RecordUpdateView.as_view(), name='record_edit'),
     path('records/<int:pk>/delete/', RecordDeleteView.as_view(), name='record_delete'),
     path('signup/', SignupView.as_view(), name='signup'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
