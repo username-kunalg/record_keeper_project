7c8837808ae23fe5b8d85cdd24eecf5b0a8d3ba2
username-kunalg <kunalg.tracencode@gmail.com>
2024-03-08 12:09:49 +0530

    some changes in recode form

--- record_keeper/models.py
+++ record_keeper/models.py
@@ -24,13 +24,14 @@ class Record(models.Model):
     name = models.CharField(max_length=100)
     date = models.DateField()
     activity = models.ForeignKey(Activity, on_delete=models.CASCADE)  # ForeignKey relationship
-    hours = models.DecimalField(max_digits=4, decimal_places=1)
-    travel = models.DecimalField(max_digits=4, decimal_places=1)
-    mileage = models.DecimalField(max_digits=5, decimal_places=1)
-    wait = models.DecimalField(max_digits=4, decimal_places=1)
+    hours = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+    travel = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+    mileage = models.DecimalField(max_digits=5, decimal_places=1, default=0)
+    wait = models.DecimalField(max_digits=4, decimal_places=1, default=0)
     expense = models.DecimalField(max_digits=10, decimal_places=2,
-                                  verbose_name='Expense ($)')  # Define verbose_name for better readability
-    comments = models.TextField(blank=True)  # Allow comments to be optional
+                                  verbose_name='Expense ($)',
+                                  default=0) # Define verbose_name for better readability
+    comments = models.TextField()  # Allow comments to be optional
 
     # Methods
     def __str__(self):
